7859e93dd69508d077b671188f43eea0e665ccb8
Chelsea <cgrindstaff@tegna.com>
2024-07-24 12:21:42 -0400

    quotes-other

---
+++ quotes-all.js
@@ -0,0 +1,132 @@
+quotes=[
+  {
+    "quote": "You need to let go. Your past doesn't define who you are. It just gives you the starting point for who you're going to be.",
+    "source": "Agent Carolina, RVB - 10x22 Don't Say It",
+    "category": "TV Shows"
+  },
+  {
+    "quote": "Be yourself and like what you like, fuck everyone else.",
+    "source": "M. Shadows",
+    "category": "People"
+  },
+  {
+    "quote": "I have loved the stars too fondly to be fearful of the night",
+    "source": "Sarah Williams, A Young Astronomer (poem), 1868",
+    "category": "Books + Comics"
+  },
+  {
+    "quote": "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+    "source": "Douglas Adams, Mostly Harmless",
+    "category": "Books + Comics"
+  },
+  {
+    "quote": "Don't be sorry, be better",
+    "source": "Kratos, God of War 4",
+    "category": "Games"
+  },
+  {
+    "quote": "If you allow your past to control you, you've already decided on what will become your future.",
+    "source": "Unknown",
+    "category": "Misc"
+  },
+  {
+    "quote": "The Dark cannot claim what Light does not surrender.",
+    "source": "C.L. Wilson, Crown of Crystal Flame",
+    "category": "Books + Comics"
+  },
+  {
+    "quote": "Who doesn't love it when the lasers go \"pew pew\"?",
+    "source": "Midnight Burger, Shift Notes, Episode 1",
+    "category": "Podcasts"
+  },
+  {
+    "quote": "Being a hero is giving a shit and doing something about it.",
+    "source": "Jessica Jones, Jessica Jones",
+    "category": "TV Shows"
+  },
+  {
+    "quote": "I imagine one of the reasons people cling to their hates so stubbornly is because they sense, once hate is gone, they will be forced to deal with pain",
+    "source": "James Baldwin, American writer and civil rights activist",
+    "category": "People"
+  },
+  {
+    "quote": "Don't die isn't a plan; it's a general demand of living.",
+    "source": "Loki, Loki",
+    "category": "TV Shows"
+  },
+  {
+    "quote": "Knowing it's real means you gotta make a decision. One, keep denying it. Or two... do something about it.",
+    "source": "Jessica Jones, Jessica Jones",
+    "category": "TV Shows"
+  },
+  {
+    "quote": "He who fears he shall suffer, already suffers what he fears",
+    "source": "Michel de Montaigne",
+    "category": "People"
+  },
+  {
+    "quote": "I'm good at everything, except the things that I can't do",
+    "source": "x, Squid Games",
+    "category": "TV Shows"
+  },
+  {
+    "quote": "I have passed through fire",
+    "source": "Critical Role, Campaign 1",
+    "category": "Podcasts"
+  },
+  {
+    "quote": "When you're angry, you're just a character in someone else's story. But when you let your anger go, you reclaim your own story. Become your own protagonist again.",
+    "source": "Kacy Duke, Inventing Anna",
+    "category": "TV Shows"
+  },
+  {
+    "quote": "Opinions are like opera... Sure, you can listen to them, but why would you really?",
+    "source": "FCG, Critical Role, Campaign 3 episode 17",
+    "category": "Podcasts"
+  },
+  {
+    "quote": "When it looks impossible, look deeper. And then fight like you can win",
+    "source": "Rost, Horizon Zero Dawn",
+    "category": "Games"
+  },
+  {
+    "quote": "There is no protocol for this.",
+    "source": "SecBot 3, MurderBot",
+    "category": "Books + Comics"
+  },
+  {
+    "quote": "If I was as ignorant as you I wouldn't let on.",
+    "source": "Huckleberry Finn",
+    "category": "Books + Comics"
+  },
+  {
+    "quote": "No book is really worth reading at the age of ten which is not equally – and often far more – worth reading at the age of fifty and beyond.",
+    "source": "CS Lewis",
+    "category": "Books + Comics"
+  },
+  {
+    "quote": "You may not control all the events that happen to you, but you can decide not to be reduced by them",
+    "source": "Maya Angelou",
+    "category": "People"
+  },
+  {
+    "quote": "I tend to be a skeptic in the daytime and a believer in the dark",
+    "source": "Trevor Collins, Red Web",
+    "category": "Podcasts"
+  },
+  {
+    "quote": "Science fiction is like science's irresponsible dumb cousin",
+    "source": "Midnight Burger, Shift Notes, Roadtrip",
+    "category": "Podcasts"
+  },
+  {
+    "quote": "You can't change the past, only how it shapes you",
+    "source": "Kratos, God of War 4",
+    "category": "Games"
+  },
+  {
+    "quote": "Sons of Gondor! Of Rohan! My brothers. I see in your eyes the same fear that would take the heart of me. A day may come when the courage of Men fails, when we forsake our friends and break all bonds of fellowship, but it is not this day. An hour of wolves and shattered shields when the Age of Men comes crashing down, but it is not this day! This day we fight! By all that you hold dear on this good earth, I bid you stand, Men of the West!",
+    "source": "Aragon, Return of the King",
+    "category": "Movies"
+  }
+];
---
+++ quotes-taylor.js
@@ -0,0 +1,22 @@
+quotes=[
+  {
+    "quote": "I can't stop you putting roots in my dreamland",
+    "song": "Ivy",
+    "album": "Evermore"
+  },
+  {
+    "quote": "I had the time of my life fighting dragons with you",
+    "song": "Long Live",
+    "album": "Speak Now"
+  },
+  {
+    "quote": "Everything you lose is a step you take",
+    "song": "You're On Your Own, Kid",
+    "album": "Midnights"
+  },
+  {
+    "quote": "I can still make the whole place shimmer",
+    "song": "Bejeweled",
+    "album": "Midnights"
+  }
+];
